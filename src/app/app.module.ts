bfbdfef66101af4df61082fb1f74197d6bfef8c1
David Sklář <david.sklar.cs@gmail.com>
2019-10-07 11:55:14 +0200

    feat: pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,9 @@ import {
 } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { ListComponent } from './list/list.component';
@@ -34,6 +36,7 @@ import { ListComponent } from './list/list.component';
         MatSnackBarModule,
         MatTableModule,
         ReactiveFormsModule,
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     ],
     providers: [
         {
